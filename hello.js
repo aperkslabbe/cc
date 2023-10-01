89c48314b464bf8b09f3522115859313c3e42caf
aperkslabbe <aperkslabbe@shutterstock.com>
2023-09-30 20:13:41 -0400

    latest

--- hello.js
+++ hello.js
@@ -1,19 +1,20 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 800, 800 ], 
-  animate: true, 
+  dimensions: [800, 800],
+  // animate: true,
   fps: 60,
 };
 
-const sketch = ({ context, width, height }) => {
-  let x = 0, y = 0;
+const sketch = ({ ctx, width, height }) => {
+  let x = 0,
+    y = 0;
 
-  context.fillStyle = 'blue';
-  context.fillRect(0, 0, width, height);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(0, 0, width, height);
 
-  return ({ context, width, height, frame }) => {
-    if (frame > 139) return; 
+  return ({ ctx, width, height, frame }) => {
+    if (frame > 139) return;
     x += 10;
 
     if (x > 460) {
@@ -21,19 +22,19 @@ const sketch = ({ context, width, height }) => {
       x = 0;
     }
 
-    context.fillStyle = (frame % 2) ? 'black' : 'white';
-    context.strokeStyle = context.fillStyle;
-    context.lineWidth = 10;
+    ctx.fillStyle = frame % 2 ? "black" : "yellow";
+    ctx.strokeStyle = ctx.fillStyle;
+    ctx.lineWidth = 10;
 
-    context.strokeRect(x + 75, y + 140, 150, 110);
-    context.fillRect(x + 130, y + 190, 40, 60);
+    ctx.strokeRect(x + 75, y + 140, 150, 110);
+    ctx.fillRect(x + 130, y + 190, 40, 60);
 
-    context.beginPath();
-    context.moveTo(x + 50,  y + 140);
-    context.lineTo(x + 150, y + 60);
-    context.lineTo(x + 250, y + 140);
-    context.closePath();
-    context.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x + 50, y + 140);
+    ctx.lineTo(x + 150, y + 60);
+    ctx.lineTo(x + 250, y + 140);
+    ctx.closePath();
+    ctx.stroke();
   };
 };
 
--- sketch-skew.js
+++ sketch-skew.js
@@ -1,54 +1,87 @@
-const canvasSketch = require('canvas-sketch');
-const math = require('canvas-sketch-util/math');
-const random = require('canvas-sketch-util/random');
+const canvasSketch = require("canvas-sketch");
+const math = require("canvas-sketch-util/math");
+const random = require("canvas-sketch-util/random");
+const color = require("canvas-sketch-util/color");
+const risoColors = require("riso-colors");
 
 const settings = {
-  dimensions: [ 1080, 1080 ], 
-  // animate: true
+  dimensions: [1080, 1080],
+  animate: true,
 };
 
 const sketch = ({ width, height }) => {
-  let x, y, w, h;
+  let x, y, w, h, fill, stroke, blend;
 
   const rects = [];
- 
-  for (let i = 0; i < 20; i++) { 
+  const colors = [random.pick(risoColors), random.pick(risoColors)];
+
+  const bgColor = random.pick(risoColors).hex;
+
+  for (let i = 0; i < 40; i++) {
     x = random.range(0, width);
     y = random.range(0, height);
-    w = random.range(200, 600);
+    w = random.range(600, width);
     h = random.range(40, 200);
-    rects.push({ x, y, w, h });
+    fill = random.pick(colors).hex;
+    stroke = random.pick(colors).hex;
+    blend = random.value() > 0.5 ? "overlay" : "source-over";
+    rects.push({ x, y, w, h, fill, stroke, blend });
   }
 
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+  return ({ context: ctx, width, height }) => {
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, width, height);
 
-    rects.forEach(({ x, y, w, h }) => {
-      context.save();
-      context.translate(x, y);
-      context.strokeStyle = 'blue';
-      drawSkewedRect({ context, w, h, degrees: -45 });
-      context.stroke();
-      context.restore();
+    rects.forEach(({ x, y, w, h, fill, stroke, blend }) => {
+      const shadowColor = color.offsetHSL(fill, 0, 0, -20);
+      shadowColor.rgba[3] = 0.5;
+
+      ctx.save();
+      ctx.translate(x, y);
+      ctx.strokeStyle = stroke;
+      ctx.fillStyle = fill;
+      ctx.lineWidth = 10;
+      ctx.globalCompositeOperation = blend;
+
+      drawSkewedRect({ ctx, w, h, degrees: -45 });
+
+      ctx.shadowColor = color.style(shadowColor.rgba);
+      ctx.shadowOffsetX = -10;
+      ctx.shadowOffsetY = 20;
+
+      ctx.fill();
+
+      ctx.shadowColor = null;
+      ctx.stroke();
+
+      ctx.globalCompositeOperation = "source-over";
+      ctx.lineWidth = 2;
+      ctx.strokeStyle = "black";
+      ctx.stroke();
+      ctx.restore();
     });
   };
 };
 
-const drawSkewedRect = ({ context, w = 600, h = 200, degrees = -90  }) => {
+const drawTriangle = ({ ctx }) => {
+  ctx.save();
+  
+}
+
+const drawSkewedRect = ({ ctx, w = 600, h = 200, degrees = -90 }) => {
   const angle = math.degToRad(degrees),
-    rx = Math.cos(angle) * w, 
+    rx = Math.cos(angle) * w,
     ry = Math.sin(angle) * w;
 
-  context.save();
-  context.translate(rx * -0.5, (ry + h) * -0.5); 
-  context.beginPath();
-  context.moveTo(0, 0);
-  context.lineTo(rx, ry); 
-  context.lineTo(rx, ry + h);
-  context.lineTo(0, h);
-  context.closePath();
-  context.restore();
-}
+  ctx.save();
+  ctx.translate(rx * -0.5, (ry + h) * -0.5);
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(rx, ry);
+  ctx.lineTo(rx, ry + h);
+  ctx.lineTo(0, h);
+  ctx.closePath();
+  ctx.restore();
+};
 
 canvasSketch(sketch, settings);
